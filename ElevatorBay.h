4a5c450dbd9c1906a7ea52786d466a4b5e76a36c
laba2346 <laba2346@users.noreply.github.com>
2016-04-20 17:03:14 -0600

    Update and rename Elevator.h to ElevatorBay.h

---
+++ ElevatorBay.h
@@ -0,0 +1,23 @@
+#ifndef ELEVATOR_H
+#define ELEVATOR_H
+#include<vector>
+#include<iostream>
+
+struct Person{
+    string identifier;
+    int floor;
+};
+
+class Elevator
+{
+    public:
+        Elevator();
+        ~Elevator();
+
+    protected:
+    private:
+
+};
+
+#endif // GRAPH_H
+
