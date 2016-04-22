68a1315c288c47979b4973827f320b5dc84c5892
Crosswise1 (D. Crosswy) <David.Crosswy@colorado.edu>
2016-04-22 14:48:45 -0600

    made corrections to ProcessingPeople method to correct seg fault

--- src/ElevatorBay.cpp
+++ src/ElevatorBay.cpp
@@ -44,7 +44,7 @@ void ElevatorBay::allocatePeople1(){ //This method allocates people for floors 1
     std::cout << "================" << std::endl; //this is just error checking
     for (unsigned int i = 0; i < people.size(); i++){
         if (people[i].floor >= 1 && people[i].floor <= 5){
-            //std::cout << "Name:: " << people[i].id <<", Floor: " << people[i].floor << std::endl;
+            std::cout << "Name:: " << people[i].id <<", Floor: " << people[i].floor << std::endl;
             e1.passengers.push(people[i]);
         }
         else if(people[i].floor >= 6 && people[i].floor <= 10){
@@ -55,26 +55,34 @@ void ElevatorBay::allocatePeople1(){ //This method allocates people for floors 1
         }
     }
     //Do we need to add a condition if queue is not empty?
-    std::cout<<"test" << std::endl;
+
     processPeople(e1);
 }
 
 void ElevatorBay::processPeople(Elevator e){
     int x = 0;
-    std::vector <int> tmp;
+    std::vector<int> tmp;
+    //std::cout<<"tmp's size: " << tmp.size() << std::endl;
     while (!e.passengers.empty()){
         //std::cout << e1.passengers.front().id << std::endl;
         int floor = e.passengers.front().floor;
+        std::cout <<"Floor number: " << floor << std::endl;
         bool found = false;
-        for (int j  = 0; j <= tmp.size(); j++){
-            if (tmp[j] == floor) {
-                found = true;
-                break;
+        if (!tmp.empty()){
+            for (unsigned int j  = 0; j <= tmp.size(); j++){
+                std::cout<<"what is J = " << j << std::endl;
+                if (tmp[j] == floor) {
+                    found = true;
+                    break;
+                }
             }
         }
+        std::cout<<"5 test" << std::endl;
         if(!found) {
             tmp.push_back(floor);
+            //std::cout<<"5 test" << std::endl;
             e.floorsVisited++;
+            //std::cout<<"6 test" << std::endl;
         }
         e.passengers.pop();
         x++;
