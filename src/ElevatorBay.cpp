5b25aeb634714c7c6dbb0c83abc194fbac06926b
Crosswise1 (D. Crosswy) <David.Crosswy@colorado.edu>
2016-04-22 12:42:15 -0600

    added new methods to allocate people and general process of counting elevator floors

--- src/ElevatorBay.cpp
+++ src/ElevatorBay.cpp
@@ -6,6 +6,7 @@
 #include<sstream>
 #include<string>
 
+
 ElevatorBay::ElevatorBay(std::string fname){
     readFile(fname);
 }
@@ -43,16 +44,42 @@ void ElevatorBay::allocatePeople1(){ //This method allocates people for floors 1
     std::cout << "================" << std::endl; //this is just error checking
     for (unsigned int i = 0; i < people.size(); i++){
         if (people[i].floor >= 1 && people[i].floor <= 5){
-            std::cout << "Name:: " << people[i].id <<", Floor: " << people[i].floor << std::endl;
+            //std::cout << "Name:: " << people[i].id <<", Floor: " << people[i].floor << std::endl;
             e1.passengers.push(people[i]);
         }
     }
-    if (e1.passengers.empty())
-        std::cout<<"queue is empty"<<std::endl;
-    else if (!e1.passengers.empty())
-        std::cout <<"QUEUE is not empty"<<std::endl;
-    //add a condition if queue is not empty?
-    while (!e1.passengers.empty()){
-        std::cout << e1.passengers.front() << std::endl;
+    //Do we need to add a condition if queue is not empty?
+    std::cout<<"test" << std::endl;
+    processPeople(e1);
+}
+
+void ElevatorBay::processPeople(Elevator e){
+    int x = 0;
+    std::vector <int> tmp;
+    while (!e.passengers.empty()){
+        //std::cout << e1.passengers.front().id << std::endl;
+        int floor = e.passengers.front().floor;
+        bool found = false;
+        for (int j  = 0; j <= tmp.size(); j++){
+            if (tmp[j] == floor) {
+                found = true;
+                break;
+            }
+        }
+        if(!found) {
+            tmp.push_back(floor);
+            e.floorsVisited++;
+        }
+        e.passengers.pop();
+        x++;
+        if (x == e.capacity-1){
+            x = 0;
+            tmp.clear();
+            e.floorsVisited++;
+        }
     }
+    if (x != 0)
+        e.floorsVisited++;
+    std::cout<<"floors visited: "<< e.floorsVisited << std::endl;
+
 }
